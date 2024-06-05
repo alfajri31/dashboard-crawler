bb5bb64384e5acc862b356ecee5a1c799f19bcaa
Alfajri <alfajri607@gmail.com>
2024-06-05 08:45:09 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import {MainDashboard} from "./components/MainDashboard";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener  noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id={"mainDashboard"}>
+          <div className={"container-fluid"}>
+              <MainDashboard/>
+          </div>
+
+      </div>
   );
 }
 
